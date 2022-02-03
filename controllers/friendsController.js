9e8f11b0ce6bed393287bbb7ba77bd57123a0f16
mauricinhoo <mauriciopereira2310@gmail.com>
2022-02-02 21:55:19 -0300

    Projeto final

--- controllers/friendsController.js
+++ controllers/friendsController.js
@@ -3,10 +3,24 @@ const router = express.Router()
 const User = require('../models/User')
 const Friends = require('../models/Friends')
 const userLogin = require("../middlewares/userLogin")
+const { route } = require('express/lib/application')
 
 router.get("/user/friends", userLogin, (req, res) => {
 
-    res.render("user/friends/friends")
+    Friends.findAll({
+        where: {
+            answer: "A",
+            idSender: req.session.user.id
+        },
+    }).then((friends) => {
+        User.findAll({
+        }).then((users) => {
+            res.render("user/friends/friends", { friends: friends, users: users, sessionUser: req.session.user })
+        }).catch((err) => {
+            console.log(err)
+        })
+    })
+
 
 })
 
@@ -19,6 +33,7 @@ router.get("/user/sent", userLogin, (req, res) => {
         },
     }).then((friends) => {
         User.findAll({
+            attributes: ['id', 'name', 'profession', 'avatar']
         }).then((users) => {
             res.render("user/friends/sent", { friends: friends, users: users })
         }).catch((err) => {
@@ -32,12 +47,13 @@ router.get("/user/pending", userLogin, (req, res) => {
     Friends.findAll({
         where: {
             answer: "P",
-            idSender: req.session.user.id
+            idRecipient: req.session.user.id
         },
     }).then((friends) => {
         User.findAll({
+            attributes: ['id', 'name', 'profession', 'avatar']
         }).then((users) => {
-            res.render("user/friends/pending", { friends: friends, users: users })
+            res.render("user/friends/pending", { friends: friends, users: users, usuario: req.session.user })
         }).catch((err) => {
             console.log(err)
         })
@@ -67,8 +83,6 @@ router.post("/user/request", userLogin, (req, res) => {
                 console.log(err)
                 res.json(err)
                 //res.redirect("/user/profile")
-                console.log(id)
-                console.log(req.session.user.id)
             })
 
         }
@@ -78,6 +92,67 @@ router.post("/user/request", userLogin, (req, res) => {
     }).catch(() => {
         //res.redirect("/user/index")
     })
+})
+
+router.post("/user/answer", userLogin, (req, res) => {
+    let { response, id } = req.body
+
+    console.log(id)
+    console.log(req.session.user.id)
+
+    id = parseFloat(id)
+
+    if (response == "accept") {
+        var answer = "A"
+    } else {
+        if (response == "decline") {
+            var answer = "D"
+        }
+        else {
+            res.redirect("/user/pending")
+        }
+    }
+    if (answer == "A") {
+        Friends.findOne({
+            where: {
+                idSender: id,
+                idRecipient: req.session.user.id
+            }
+        }).then(user => {
+            if (user != undefined) {
+                Friends.update({
+                    answer: answer
+                }, {
+                    where: {
+                        idSender: id,
+                        idRecipient: req.session.user.id
+                    }
+                }).then(() => {
+                    res.redirect("/user/pending")
+                }).catch(err => {
+                    res.redirect("/user/pending")
+                })
+            }
+            else {
+                res.redirect("/user/pending")
+            }
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+    else {
+        Friends.destroy({
+            where: {
+                idSender: id,
+                idRecipient: req.session.user.id
+            }
+        }).then(() => {
+            res.redirect("/user/pending")
+        }).catch(err => {
+            res.redirect("/user/pending")
+            console.log(err)
+        })
+    }
 })
 
 
